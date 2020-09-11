7dafd24f4595f8c132edd8defe8f0798a5cbe8a1
Anniel Menezes Cruz <contato@annielmenezes.com>
2020-09-10 21:15:59 -0300

    feat: add covid page

--- pages/covid.js
+++ pages/covid.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import { Box, Container as MDContainer, Typography } from '@material-ui/core'
+import axios from 'axios'
 import Container from '../components/container'
+import { Chart } from '../src/molecules'
 import { Layout } from '../src/templates/layout'
-import { Header } from '../src/organisms'
+import { SinteseComponent } from '../src/molecules/covid-sintese/covid-sintese'
+import { tsvJSON } from '../src/helpers/tsv-to-json'
 
 export default function Index() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('/covid-data.tsv').then((res) => {
+      setData(tsvJSON(res?.data))
+    })
+  }, [setData])
   return (
     <Layout>
       <Head>
@@ -12,7 +24,42 @@ export default function Index() {
           classificados e muito mais...
         </title>
       </Head>
-      <Container>Data here</Container>
+      <Container>
+        <MDContainer>
+          <Box
+            display="flex"
+            flexDirection="column"
+            mt={4}
+            style={{ marginTop: 10 }}
+          >
+            <Box
+              display="flex"
+              justifyContent="flex-end"
+              alignItems="center"
+              flexWrap="wrap"
+            >
+              <Typography
+                variant="body1"
+                style={{ fontSize: '1rem', color: 'rgba(0, 0, 0, 0.54)' }}
+              >
+                Última atualização: {data[data.length - 1]?.data} às{' '}
+                {data[data.length - 1]?.hora}
+              </Typography>
+            </Box>
+            <Box mt={2}>
+              <SinteseComponent data={data} />
+            </Box>
+            <Box my={4}>
+              {data && (
+                <Chart
+                  data={data}
+                  title="Casos de covid por data de notificação"
+                />
+              )}
+            </Box>
+          </Box>
+        </MDContainer>
+      </Container>
     </Layout>
   )
 }
